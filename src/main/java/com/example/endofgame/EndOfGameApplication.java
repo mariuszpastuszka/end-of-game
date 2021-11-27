e160c11dfd651bc47ae384d0e0927d941608515a
maniek <mpastuszka@gmail.com>
2021-11-27 09:30:25 +0100

    [init] - initial commit

---
+++ src/main/java/com/example/endofgame/EndOfGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.endofgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndOfGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndOfGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/endofgame/EndOfGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endofgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndOfGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
