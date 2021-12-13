c7f5144e6d1c5ec191aa5407610f370c4d9a0a16
maniek <mpastuszka@gmail.com>
2021-12-13 19:58:18 +0100

    [add] - template for all categories created

--- src/main/java/com/example/endofgame/controller/web/CategoryWebController.java
+++ src/main/java/com/example/endofgame/controller/web/CategoryWebController.java
@@ -1,12 +1,28 @@
 package com.example.endofgame.controller.web;
 
+import com.example.endofgame.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("/web")
 public class CategoryWebController {
 
+    private static final String CATEGORIES_KEY = "categories";
+    private final CategoryService categoryService;
 
+    public CategoryWebController(final CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/all-categories")
+    public String allCategories(Model model) {
+        model.addAttribute(CATEGORIES_KEY, categoryService.readAllCategories());
+
+        return "categories/all-categories";
+    }
 }
