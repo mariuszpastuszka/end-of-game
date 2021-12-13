5fc78f48333e9b337a128e2d56645e608324487c
maniek <mpastuszka@gmail.com>
2021-12-13 20:32:54 +0100

    [add] - deletion of category by web page implemented

--- src/main/java/com/example/endofgame/controller/web/CategoryWebController.java
+++ src/main/java/com/example/endofgame/controller/web/CategoryWebController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +25,15 @@ public class CategoryWebController {
         model.addAttribute(CATEGORIES_KEY, categoryService.readAllCategories());
 
         return "categories/all-categories";
+    }
+
+    // delete-category/{id}
+    @GetMapping("delete-category/{id}")
+    public String deleteCategoryById(@PathVariable("id") Long id) {
+        log.info("deleting category by id: [{}]", id);
+
+        categoryService.deleteCategoryById(id);
+
+        return "redirect:/web/all-categories";
     }
 }
