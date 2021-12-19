bb69d54dafc68d2f2f15bad650b3bc9d77c04d36
maniek <mpastuszka@gmail.com>
2021-12-19 10:48:21 +0100

    [add] - category service completed

--- angular-ui/src/app/services/category.service.ts
+++ angular-ui/src/app/services/category.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category';
+import { categoriesApi } from '../models/links';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  readCategoriesFromServer(): Observable<Array<Category>> {
+    return this.httpClient.get<Array<Category>>(categoriesApi);
+  }
 }
