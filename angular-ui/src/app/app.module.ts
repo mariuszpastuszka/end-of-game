4e0087048178434ebc9fe976f1a8bf9778dc3fe3
maniek <mpastuszka@gmail.com>
2021-12-19 13:10:26 +0100

    [change] - better look of table of categories

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CategoryComponent } from './components/category/category.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-ui/src/app/components/category/category.component.ts
+++ angular-ui/src/app/components/category/category.component.ts
@@ -10,12 +10,18 @@ import { Category } from '../../models/category';
 export class CategoryComponent implements OnInit {
 
   categories: Array<Category> = [];
+  displayedColumns: string[] = ["id", "name", "cr", "up"];
+  dataSource = this.categories;
 
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.categoryService.readCategoriesFromServer()
-      .subscribe(categoriesFromServer => this.categories = categoriesFromServer);
+      .subscribe(categoriesFromServer => {
+        this.categories = categoriesFromServer
+        this.dataSource = this.categories
+        console.log(`data received from server, number of elements is: [${categoriesFromServer.length}]`)
+      });
   }
 
 }
